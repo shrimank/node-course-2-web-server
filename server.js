ec05a3bddb6dd9c35d893cafe84c0c4c5cb5e3c6
shrimank <shrimankumbar@gmail.com>
2018-01-14 18:30:08 +0530

    port dynamically added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
-
+const port = process.env.PORT ||  3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine',hbs);
@@ -49,6 +49,6 @@ app.get('/bad',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-  console.log('Server is up and listening 3000');
+app.listen(port,()=>{
+  console.log(`Server is up and listening ${port}`);
 });
